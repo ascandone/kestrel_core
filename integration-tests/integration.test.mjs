c6c0ac219ca6ebca3030041dacfd95893626555c
Alessandro Scandone <alescandone@gmail.com>
2024-04-03 15:23:46 +0200

    edited CI

--- integration-tests/integration.test.mjs
+++ integration-tests/integration.test.mjs
@@ -5,17 +5,21 @@ import { promisify } from "node:util";
 import { exec as exec_ } from "node:child_process";
 import { readdirSync } from "node:fs";
 
+const wdir = "integration-tests";
+
 const exec = promisify(exec_);
 
-const content = readdirSync("src")
+const content = readdirSync(`${wdir}/src`)
   .filter((f) => f.endsWith("Tests.kes"))
   .map((f) => f.replace(".kes", ""));
 
+assert(content.length !== 0, "empty test suite");
+
 for (const fileName of content) {
   test(fileName, async () => {
-    const { stdout } = await exec(`kestrel run ${fileName}`);
+    const { stdout } = await exec(`kestrel run ${fileName}`, { cwd: wdir });
 
-    const expected = await readFile(`src/${fileName}.out`);
+    const expected = await readFile(`${wdir}/src/${fileName}.out`);
     assert.strictEqual(stdout, expected.toString());
   });
 }
