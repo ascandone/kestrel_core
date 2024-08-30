d50e66e60eaf8abdbb4c8118efa5f4e7c8a948ec
ascandone <alescandone@gmail.com>
2024-08-30 23:51:59 +0200

    added set data structure

--- src/Bool.js
+++ src/Bool.js
@@ -6,3 +6,7 @@ const Eq_Char$Char = (x, y) => x === y;
 const Eq_Bool$Bool = (x, y) => x === y;
 
 const _eq = (Eq) => (x, y) => Eq(x, y);
+
+const Ord_Int$Int = (x, y) => x < y;
+const Ord_Float$Float = (x, y) => x < y;
+const Ord_String$String = (x, y) => x < y;
