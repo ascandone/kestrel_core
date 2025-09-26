10c35ce3df565994664028aa0c206aeb694a3c59
Alessandro Scandone <alescandone@gmail.com>
2025-09-26 10:03:54 +0200

    moved show to String module

--- src/String.js
+++ src/String.js
@@ -61,3 +61,31 @@ function String$cons(ch, str) {
 function String$from_char(ch) {
   return String$cons(ch, "");
 }
+
+const String$show = (Show) => (x) => Show(x);
+const Show_Int$Int = (n) => n.toString();
+const Show_Float$Float = (n) => {
+  if (Math.round(n) === n) {
+    return `${n}.0`;
+  }
+
+  return n.toString();
+};
+const Show_String$String = (s) => `"${s}"`;
+const Show_Char$Char = (c) => `'${c}'`;
+
+const Show_Bool$Bool = (n) => {
+  if (n) {
+    return "True";
+  }
+  return "False";
+};
+
+const Show_List$List = (Show_a) => (x) => {
+  const buf = [];
+  while (x.$ != 0) {
+    buf.push(Show_a(x._0));
+    x = x._1;
+  }
+  return `[${buf.join(", ")}]`;
+};
